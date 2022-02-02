e90e9b14e1ac5cef78f2cfbb3efffbd711b9db6a
Gouthum Karadi <karadi@virtualnex.us>
2022-02-02 15:26:13 -0400

    Success. Needed new Alchemy URL.

--- signTransactions.js
+++ signTransactions.js
@@ -8,14 +8,15 @@ const common = new Common({chain: Chain.Rinkeby, hardfork: Hardfork.London });
 const PRIVATE_KEY = process.env.PRIVATE_KEY; // if you set up .dotenv file correctly, your private key will be accessed automatically using `process.env`
 
 // Add your Alchemy/Infura endpoint (make sure it is a Rinkeby endpoint for this one!)
-const ALCHEMY_ENDPOINT = "";
+const ALCHEMY_ENDPOINT = "https://eth-rinkeby.alchemyapi.io/v2/fW85qYYKRCsOGJFKPS6enz8pt0-_LfId";
 
 const txParams = {
-  "nonce": 0, // you will need to change this!
+  //"from": "0x96A83D6a7e0E88a741C64C5BE559E7293254115B",
+  "nonce": 20, // you will need to change this!
   "gasLimit": "0x5208",
   "maxPriorityFeePerGas": "0x3b9aca00",
   "maxFeePerGas": "0x3b9acaff",
-  "to": "", // choose someone to send some ETH to!
+  "to": "0x2767CD05FdC45389aDb55e016358003227CfeBA5", // choose someone to send some ETH to!
   "value": "0x5af3107a4000", // .0001 ether
   "chainId": "0x04", // rinkeby chain id
   "type": "0x02" // eip 1559
